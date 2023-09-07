b536f033e65b8328a58d33ed2b246c91cd2ab1fb
Sonuuarya <132288685+Sonuuarya@users.noreply.github.com>
2023-09-07 13:32:46 +0530

    Update config.py

--- NoteNinja/config.py
+++ NoteNinja/config.py
@@ -1,26 +1,27 @@
-
 class Config(object):
     LOGGER = True
-   
-    API_ID = "22018080" 
-    API_HASH = "1cc4bac38c10d11ff9a68880cbdc1af3"
-    TOKEN = "6309557731:AAFPD-48uzhXRqUFZYDku3FrxTg7RB55q90"  
-    OWNER_ID = 5003680905
+    # REQUIRED
+    # Login to https://my.telegram.org and fill in these slots with the details given by it
+
+    API_ID = "" # integer value, dont use ""
+    API_HASH = ""
+    TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 2145093972 # If you dont know, run the bot and do /id in your private chat with it, also an integer
     
-    SUPPORT_CHAT = "NoteNinjaSupport" 
-    START_IMG = "https://graph.org/file/aba5d5d746926a493bd26.jpg"
-    EVENT_LOGS = ()  
-    MONGO_DB_URI= "mongodb+srv://USER_ID:PASSWORD@user.m23k7t3.mongodb.net/?retryWrites=true&w=majority"
+    SUPPORT_CHAT = "the_support_chat"  # Your own group for support, do not add the @
+    START_IMG = ""
+    EVENT_LOGS = ()  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+    MONGO_DB_URI= ""
     # RECOMMENDED
-    DATABASE_URL = "postgres://ddowxvgf:mPQIhSjJmXRQyGhU7OMKVjeACqpFltBT@dumbo.db.elephantsql.com/ddowxvgf"  # A sql database url from elephantsql.com
+    DATABASE_URL = ""  # A sql database url from elephantsql.com
     CASH_API_KEY = (
-        "GVUHVUJYKI8C693V"  # Get your API key from https://www.alphavantage.co/support/#api-key
+        ""  # Get your API key from https://www.alphavantage.co/support/#api-key
     )
-    TIME_API_KEY = "42RTE6OVQJK2"
+    TIME_API_KEY = ""
     # Get your API key from https://timezonedb.com/api
 
     # Optional fields
-    CHATBOT_API="6969392047:FheF6ep48anTeyep" # get it from @FallenChat_Bot using /token
+    CHATBOT_API="" # get it from @FallenChat_Bot using /token
     BL_CHATS = []  # List of groups that you want blacklisted.
     DRAGONS = []  # User id of sudo users
     DEV_USERS = []  # User id of dev users
