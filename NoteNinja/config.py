0f619ac033d450f5e8ca3477d8bc63f13582e57d
Sonuuarya <132288685+Sonuuarya@users.noreply.github.com>
2023-09-07 11:51:14 +0530

    Update config.py

--- NoteNinja/config.py
+++ NoteNinja/config.py
@@ -4,15 +4,15 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = "" # integer value, dont use ""
-    API_HASH = ""
-    TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 2145093972 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    API_ID = "22018080" # integer value, dont use ""
+    API_HASH = "1cc4bac38c10d11ff9a68880cbdc1af3"
+    TOKEN = "6309557731:AAFPD-48uzhXRqUFZYDku3FrxTg7RB55q90"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 5003680905 # If you dont know, run the bot and do /id in your private chat with it, also an integer
     
     SUPPORT_CHAT = "the_support_chat"  # Your own group for support, do not add the @
     START_IMG = ""
     EVENT_LOGS = ()  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
-    MONGO_DB_URI= ""
+    MONGO_DB_URI= "mongodb+srv://USER_ID:PASSWORD@user.m23k7t3.mongodb.net/?retryWrites=true&w=majority"
     # RECOMMENDED
     DATABASE_URL = ""  # A sql database url from elephantsql.com
     CASH_API_KEY = (
